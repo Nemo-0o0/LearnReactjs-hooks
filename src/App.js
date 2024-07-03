cafdbea919aaea584218dff427f5eabfff4b6818
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-04 00:19:20 +0700

    useState-Random

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 import {useState} from 'react'
 import './App.css';
 
-/// tinh Tong
-// const orders = [100 , 200 , 300]
+
+const gifts = [
+  'CPU CORE I9',
+  'RAM 100TB',
+  'HDD 100TB',
+  'SSD 100TB',
+]
 function App() {
-  const [info , setInfo] = useState({
-    name : 'Vi',
-    age : 20,
-    address : 'Ha Noi'
-  })
 
-  const handleUpdate = () => {
-    setInfo({
-     ...info,
-      bio : 'Thich An Chom Chom'
-    })
+  const [gift, setGift] = useState()
+
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length)
+
+    setGift(gifts[index])
   }
-  /////////////////////////////////////////////////////
-
-  // const [count, setCount] = useState(() => {
-  //   const total = orders.reduce((total, cur) => total + cur)
-  //   console.log(total)
-  //   return total
-  // })
-
-  // const handleCrease = () => {
-  //   setCount(count + 1)
-  // }
-  // Cong chi 1 lan
-
-
-  // Cong nhieu lan
-  // const handleCrease = () => {
-  //   setCount(prev => prev + 1)
-  // }
-/////////////////////////////////////////////////
+ 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <h1>{count}</h1>
-        <button onClick={handleCrease}>+</button> */}
-        <h1>{JSON.stringify(info)}</h1>
-        <button onClick={handleUpdate}>Update</button>
+        <h1>{gift || 'Chua co Phan Thuong'}</h1>
+        <button onClick={randomGift}>LAy Thuong</button>
       </header>
     </div>
   );
