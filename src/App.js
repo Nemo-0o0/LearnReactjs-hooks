1e5483d0f8314b7762700ec8c0cdfae883a6714d
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-04 03:02:40 +0700

    Two-way binding input

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import {useState} from 'react'
 import './App.css';
 
-
-const gifts = [
-  'CPU CORE I9',
-  'RAM 100TB',
-  'HDD 100TB',
-  'SSD 100TB',
-]
 function App() {
 
-  const [gift, setGift] = useState()
+  const [name ,setName] = useState('')
+  const [email ,setEmail] = useState('')
 
-  const randomGift = () => {
-    const index = Math.floor(Math.random() * gifts.length)
-
-    setGift(gifts[index])
+  const handleSubmit = () => {
+    console.log({
+      name,
+      email
+    })
   }
- 
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{gift || 'Chua co Phan Thuong'}</h1>
-        <button onClick={randomGift}>LAy Thuong</button>
+        <input 
+        value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input 
+        value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <button onClick={handleSubmit}>change</button>
       </header>
     </div>
   );
