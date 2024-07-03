03ffab178257e7706e9fa7c6a418f127aac34b71
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-04 03:55:52 +0700

    TodoList useState

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ function App() {
   const [ jobs, setJobs] = useState(() => {
     const storageJobs = JSON.parse(localStorage.getItem('jobs'))
     console.log(storageJobs)
-    return storageJobs
+    return storageJobs ?? []
   })
 
   console.log(job)
 
+  // Render jobs
   const handleSubmit = () => {
     setJobs(prev => {
 
@@ -27,6 +28,12 @@ function App() {
       return newJobs
     })
     setJob('')
+  }
+
+  // clear Job
+  const handleClear = () => {
+    localStorage.removeItem('jobs')
+    setJobs([])
   }
 
   /// KeyEnter Add
@@ -45,6 +52,7 @@ function App() {
         onKeyPress={handleKey}
         />
         <button onClick={handleSubmit}>ADD</button>
+        <button onClick={handleClear}>Clear</button>
         <ul>
           {jobs.map((job, index) => (
             <li key={index}>{job}</li>
