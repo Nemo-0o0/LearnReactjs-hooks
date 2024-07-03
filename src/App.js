31572efec347c3ac81c0ce005600a30f940d9825
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-04 00:10:24 +0700

    useState-hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
+/// tinh Tong
+// const orders = [100 , 200 , 300]
 function App() {
+  const [info , setInfo] = useState({
+    name : 'Vi',
+    age : 20,
+    address : 'Ha Noi'
+  })
+
+  const handleUpdate = () => {
+    setInfo({
+     ...info,
+      bio : 'Thich An Chom Chom'
+    })
+  }
+  /////////////////////////////////////////////////////
+
+  // const [count, setCount] = useState(() => {
+  //   const total = orders.reduce((total, cur) => total + cur)
+  //   console.log(total)
+  //   return total
+  // })
+
+  // const handleCrease = () => {
+  //   setCount(count + 1)
+  // }
+  // Cong chi 1 lan
+
+
+  // Cong nhieu lan
+  // const handleCrease = () => {
+  //   setCount(prev => prev + 1)
+  // }
+/////////////////////////////////////////////////
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <h1>{count}</h1>
+        <button onClick={handleCrease}>+</button> */}
+        <h1>{JSON.stringify(info)}</h1>
+        <button onClick={handleUpdate}>Update</button>
       </header>
     </div>
   );
