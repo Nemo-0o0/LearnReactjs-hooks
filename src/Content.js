7bba535974b49c492dedf732a4b1362df4483168
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-05 03:52:44 +0700

    useEffect with dependencies

--- src/Content.js
+++ src/Content.js
@@ -31,32 +31,42 @@ import { useEffect, useState } from 'react'
 //----------------------------------------
 // 1. Callback luon duoc goi sau khi component mounted
 
+
+const tabs = ['posts', 'comments','albums'];
 function Content() {
 
     const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
+    const [type, setType] = useState('posts')
 
-    // useEffect(() => {
-    //     document.title = title
-    // })
+    console.log(type)
 
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
             .then(res => res.json())
             .then(posts => {
                 setPosts(posts)
             })
-    }, [])
+    }, [type])
 
     return ( 
         <div>
+            {tabs.map(tab => (
+                <button 
+                key={tab}
+                style={type === tab ? {color: "#000", background: 'red'} : {}}
+                onClick={() => setType(tab)}
+                >{tab}
+                </button>
+            ))}
+
             <input 
                 value={title}
                 onChange={e => setTitle(e.target.value)}
             />
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
         </div>
