a3c27bc60ccb63f90d028a02b3e1b63ad39b439b
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-05 18:23:08 +0700

    useEffect with DOM events - scroll

--- src/Content.js
+++ src/Content.js
@@ -21,23 +21,24 @@ import { useEffect, useState } from 'react'
     -clear timer
  * 
 //---------------------------------------------------------------------------------------------------
-    // useEffect(Callback)
+    // 1.useEffect(Callback)
     // - Goi callback moi khi component re-render
         -Goi callback sau khi component them element vao DOM
-    // useEffect(Callback, [])
+    // 2.useEffect(Callback, [])
         - Chi goi callback 1 lan sau khi component mounted
-    // useEffect(Callback, [deps])
+    // 3.useEffect(Callback, [deps])
  */
 //----------------------------------------
 // 1. Callback luon duoc goi sau khi component mounted
+// 2. Cleanup Function luon duoc goi truoc khi component unmouted
 
 
 const tabs = ['posts', 'comments','albums'];
 function Content() {
 
-    const [title, setTitle] = useState('')
     const [posts, setPosts] = useState([])
     const [type, setType] = useState('posts')
+    const [showGoToTop, setShowGoToTop] = useState(false)
 
     console.log(type)
 
@@ -49,6 +50,35 @@ function Content() {
             })
     }, [type])
 
+    useEffect(() => {
+
+        const handleScroll = () => {
+            console.log(window.scrollY)
+            if (window.scrollY >= 200) {
+                // show
+                setShowGoToTop(true)
+                console.log('set State')
+            } else {
+                // hiden
+                setShowGoToTop(false)
+                
+                
+            }
+
+            // setShowGoToTop(window.scrollY >= 200)
+            // có thể dùng cách này thay cho If else
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        //CleanUp function
+        return () => {
+            console.log('Umounting....')
+            window.removeEventListener('scroll', handleScroll)
+        }
+
+    }, [])
+
     return ( 
         <div>
             {tabs.map(tab => (
@@ -59,16 +89,16 @@ function Content() {
                 >{tab}
                 </button>
             ))}
-
-            <input 
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-            />
             <ul>
                 {posts.map(post => (
                     <li key={post.id}>{post.title || post.name}</li>
                 ))}
             </ul>
+            {showGoToTop && (
+                <button style={{position: 'fixed', right: 20, bottom:20, background: 'red'}}>
+                   Go To Top 
+                </button>
+            )}
         </div>
      );
 }
