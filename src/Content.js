a8c1655705bef09da72297e8a6882aadc5489f16
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-05 22:24:20 +0700

    useEffect with fake Chat App

--- src/Content.js
+++ src/Content.js
@@ -1,67 +1,53 @@
 import { useEffect, useState } from 'react'
 
-// Events: Add / Remove event listener
-// Observer pattern; Subscribe / unsubscribe
-// Closure
-// timer: setInterval, setTimeout, clearInterval, clearTimeout
-// useState
-//Mounted / unMounted
-// ===
-// Call API
 
-/**
- 1.Update DOM
-    - F8 blog title
-2. Call API
-3.Listen DOM event
-`   -Scroll 
-    -Resize
-4. Cleanup
-    -Remove listener / unsubscribe
-    -clear timer
- * 
-//---------------------------------------------------------------------------------------------------
-    // 1.useEffect(Callback)
-    // - Goi callback moi khi component re-render
-        -Goi callback sau khi component them element vao DOM
-    // 2.useEffect(Callback, [])
-        - Chi goi callback 1 lan sau khi component mounted
-    // 3.useEffect(Callback, [deps])
- */
-//----------------------------------------
-// 1. Callback luon duoc goi sau khi component mounted
-// 2. Cleanup Function luon duoc goi truoc khi component unmouted
-// 3. Cleanup Function luon duoc goi truoc khi callback duoc goi (tru lan mounted)
+
+const  lessons = [
+    {
+        id: 1,
+        name: 'Lesson 1'
+    },
+    {
+        id: 2,
+        name: 'Lesson 2'
+    },
+    {
+        id: 3,
+        name: 'Lesson 3'
+    },
+]
 
 
 function Content() {
-    const [avatar, setAvatar] = useState()
+    const [lessonId, setLessonId] = useState(1)
 
+  
     useEffect(() => {
-        // cleanup
-
-        return () => {
-            avatar && URL.revokeObjectURL(avatar.preview)
+        const handleComment = ({ detail }) => {
+            console.log(detail)
         }
-    }, [avatar])
+    
+        
+        window.addEventListener(`lesson-${lessonId}`,handleComment)
 
-    const handlePreviewAvatar = (e) => {
-        const file = e.target.files[0]
-
-        file.preview = URL.createObjectURL(file)
-
-        setAvatar(file)
-    }
+        //cleanup
+        return () => {
+            window.removeEventListener(`lesson-${lessonId}`,handleComment)
+        }
+    }, [lessonId])
 
     return ( 
         <div>
-            <input 
-            type='file'
-            onChange={handlePreviewAvatar}
-            />
-            {avatar && (
-                <img src={avatar.preview} alt="" width='80%' />
-            )}
+            <ul>
+                {lessons.map(lesson => (
+                    <li key={lesson.id}
+                        style={{color: lessonId === lesson.id ? 'red': '#fff'}}
+                        onClick={() => setLessonId(lesson.id)}
+                    >
+                        {lesson.name}
+                    </li>
+                ))}
+            </ul>
         </div>
      );
 }
