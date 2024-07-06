77b9e3cde1a16bd88c49fda377da93aaa5d428b0
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-06 07:12:17 +0700

    React.memo HOC

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useState, memo } from "react";
 import "./App.css";
 import Content from "./Content";
 
-// Mounted / Unmounted
+// 1. memo() -> Higher Order Component (HOC)
+// 2. usCallback()
+
+// Hooks
+// HOC
+// Render props
 
 function App() {
 
+  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Content />
-      </header>
-    </div>
-  );
+  const handleClick = () => {
+      setCount(count + 1)
+  }
+
+   return ( 
+       <div className="App-header"> 
+       <Content count={count}/>
+           <h1>{count}</h1>
+           <button onClick={handleClick}>Click me</button>
+       </div>
+    );
+
 }
 
 export default App;
--- src/Content.js
+++ src/Content.js
@@ -1,49 +1,9 @@
-import { useEffect, useRef, useState } from 'react'
-
-// lưu các giá trị qua 1 tham chiếu bên ngoài
-// Function component 
-
-function Content() {
-    
-    const [count, setCount] = useState(60)
-
-    const timerId = useRef()
-    const prevCount = useRef()
-    const h1Ref = useRef()
-
-    useEffect(() => {
-        prevCount.current = count
-    }, [count])
-
-
-    // Log DOMRect
-    useEffect(() => {
-        const rect = h1Ref.current.getBoundingClientRect()
-
-        console.log(rect);
-    })
- 
-    const handleStart = () => {
-        timerId.current = setInterval(() => {
-            setCount(prevCount => prevCount - 1)
-        }, 1000)
-
-        console.log('start' , timerId.current)
-    }
-    
-    const handleStop = () => {
-        clearInterval(timerId.current)
-        console.log('stop' , timerId.current)
-    }
-    console.log(count, prevCount.current)
+import { memo } from 'react'
+function Content({ count }) {
 
     return ( 
-        <div>
-            <h1 ref={h1Ref}>{count}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
-        </div>
+        <h2>Hien Thi: {count} </h2>
      );
 }
 
-export default Content;
+export default memo(Content);
