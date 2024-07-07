86624183781504c0fe2c68a3df6fb3c5d6952608
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-08 02:21:50 +0700

    Context + useReducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import Content from "./Content";
-import { useContext } from "react";
-import { ThemeContext } from "./ThemeContext";
-import "./App.css";
-
-// Context
-// CompA => CompB => CompC
-
-// theme: Dark / Light
+import './App.css'
+import { useStore , actions} from './store';
 
 function App() {
-  const context = useContext(ThemeContext)
-  return (
-      <div className="App-header">
-        <button onClick={context.toggleTheme}>Toggle theme</button>
-        <Content />
-      </div>
-  );
+
+  const [state, dispatch] = useStore()
+  const { todos, todoInput } = state
+
+  const handleAdd = () => {
+    dispatch(actions.addTodo(todoInput))
+  }
+
+  console.log(todos)
+
+  console.log('todoInput:', todoInput);
+
+  return ( 
+    <div className="App-header"> 
+      <input
+        value={todoInput}
+        placeholder='Enter todo'
+        onChange={e => {
+          dispatch(actions.setTodoInput(e.target.value))
+        }}
+      />
+      <button onClick={handleAdd}>Click</button>
+      {todos.map((todo, index) => ( 
+        <li key={index}>{todo}</li>
+      ))}
+    </div>
+   );
 }
 
 export default App;
